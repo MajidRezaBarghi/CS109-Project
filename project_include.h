1857e07c94dca9740adc7d84ebc46e6383283a2e
mshehan <mshehan@my.smccd.edu>
2017-02-24 15:05:49 -0800

    this include contains many. use instead of including tons of files

--- project_include.h
+++ project_include.h
@@ -5,7 +5,10 @@
 //  Created by Matthew Shehan on 2/24/17.
 //  Copyright © 2017 Matthew Shehan. All rights reserved.
 //
-
+/*
+ * This file contains a few common include statments. Include this file
+ * or add to it instead of including in multiples everywhere. 
+ */
 #ifndef project_include_h
 #define project_include_h
 #include <iostream>
