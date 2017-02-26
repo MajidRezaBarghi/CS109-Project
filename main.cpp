3a08d5c960d94854f24506237cc196c88fd39f82
mshehan <mshehan@my.smccd.edu>
2017-02-25 21:59:57 -0800

    here is my main and test file to try

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  RKTransaction
+//
+//  Created by Matthew Shehan on 2/23/17.
+//  Copyright © 2017 Matthew Shehan. All rights reserved.
+//
+
+#include <iostream>
+#include "RFITransaction.h"
+
+int main(int argc, const char * argv[]) {
+  
+  try{
+    RFITransaction::LOAD("/Users/Matthew/cmps109/hw3/RKTransaction/RKTransaction/File");
+  }
+  
+  catch(const char* error){
+    std::cout << error;
+  }
+  return 0;
+}
