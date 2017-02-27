6d7692414d7878d3ca1f25d2835e1f96b78afb38
Lize Calderon <liclader@ucsc.edu>
2017-02-27 13:44:52 -0800

    Updated KRBase

--- KRBase.h
+++ KRBase.h
@@ -2,21 +2,12 @@
 
 class KRBase {
    private:
-      void KRBase::addFact(std::vector<std::string> &facts){
-         std::string fact_key = facts.back();
-         std::size_t args_key = facts.size();
-  
-         FactBase[fact_key][args_key].push_back(facts);
-         return;
-      } 
-
-
-      void KRBase::addRule(std::vector<std::string>& rules){
-         std::string rule_key = rules.back();
-         rules.pop_back();
-         RuleBase[rule_key] = rules; 
-      }
+      void addFact(std::vector<std::string>& facts);
+      void addRule(std::vector<std::string>& rules);
+      std::vector<std::string> filterInference(const std::string fact_name, std::string arg1, ...);
+      std::vector<std::string> queryFacts(const std::vector<std::string>& query);
 
    public:   
-
+      std::map<std::string,std::map<std::size_t,std::vector<std::vector<std::string>>>> FactBase;
+      std::map<std::string,std::vector<std::string>> RuleBase;
 }
