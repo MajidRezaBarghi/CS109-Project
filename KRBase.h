146143492375a9454267b25a4ef21c47a6f31d57
Lize Calderon <liclader@ucsc.edu>
2017-02-27 13:38:32 -0800

    KRBase update w/Matthews fucntions

--- KRBase.h
+++ KRBase.h
@@ -2,7 +2,20 @@
 
 class KRBase {
    private:
+      void KRBase::addFact(std::vector<std::string> &facts){
+         std::string fact_key = facts.back();
+         std::size_t args_key = facts.size();
+  
+         FactBase[fact_key][args_key].push_back(facts);
+         return;
+      } 
 
+
+      void KRBase::addRule(std::vector<std::string>& rules){
+         std::string rule_key = rules.back();
+         rules.pop_back();
+         RuleBase[rule_key] = rules; 
+      }
 
    public:   
 
